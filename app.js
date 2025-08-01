6f7c13ad4769957695d3dd7242196a9a85ed9e52
NooberThanYall <loudervibes@gmail.com>
2025-08-01 21:30:04 +0330

    very small ass changes

--- app.js
+++ app.js
@@ -19,6 +19,76 @@ import mongoose from "mongoose";
 import http from "http";
 import cors from 'cors';
 
+
+class Application {
+  #app = express();
+  #PORT;
+  #DB_URI;
+  
+  constructor(DB_URI, PORT) {
+    this.#PORT = PORT;
+    this.#DB_URI = DB_URI;
+    this.configureAplication();
+    this.connectDB();
+    this.createServer();
+    this.createRoute();
+    configDotenv();
+  }
+  
+  configureAplication() {
+    const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+    this.#app.use(
+      cors({
+        origin: function (origin, callback) {
+          if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+          } else {
+            callback(new Error("Not allowed by CORS"));
+          }
+        },
+        credentials: true,
+      })
+    );
+    this.#app.use(json());
+    this.#app.use(morgan("dev"));
+    this.#app.use(cookieParser());
+  }
+  
+  connectDB() {
+    //   handle the connection to mongoose if some thing happens like crash should db close on exit
+    mongoose.connect(this.#DB_URI);
+    // listen on error
+    mongoose.connection.on("error", (err) => {
+      console.log(err);
+      process.exit(1);
+    });
+    // listen on connection and print connected message
+    mongoose.connection.on("connected", (data) => {
+      console.log("connected to DB");
+    });
+    // on SIGINT signal exist
+    process.on("SIGINT", () => {
+      mongoose.connection.close(() => {
+        console.log("Mongoose disconnected on app termination");
+        process.exit(0);
+      });
+    });
+  }
+  
+  createServer() {
+    http.createServer(this.#app).listen(this.#PORT);
+    if (process.env.app_state == "dev") {
+      console.log(`run on > http://localhost:${this.#PORT}`);
+    }
+  }
+  
+  createRoute() {
+    this.#app.use("/", allRoutes);
+  }
+}
+
+export default Application;
+
 // configDotenv();
 
 // const app = express();
@@ -59,72 +129,3 @@ import cors from 'cors';
 // });
 
 // export default app;
-
-class Application {
-  #app = express();
-  #PORT;
-  #DB_URI;
-
-  constructor(DB_URI, PORT) {
-    this.#PORT = PORT;
-    this.#DB_URI = DB_URI;
-    this.configureAplication();
-    this.connectDB();
-    this.createServer();
-    this.createRoute();
-    configDotenv();
-  }
-
-  configureAplication() {
-    const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
-    this.#app.use(
-      cors({
-        origin: function (origin, callback) {
-          if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-          } else {
-            callback(new Error("Not allowed by CORS"));
-          }
-        },
-        credentials: true,
-      })
-    );
-    this.#app.use(json());
-    this.#app.use(morgan("dev"));
-    this.#app.use(cookieParser());
-  }
-
-  connectDB() {
-    //   handle the connection to mongoose if some thing happens like crash should db close on exit
-    mongoose.connect(this.#DB_URI);
-    // listen on error
-    mongoose.connection.on("error", (err) => {
-      console.log(err);
-      process.exit(1);
-    });
-    // listen on connection and print connected message
-    mongoose.connection.on("connected", (data) => {
-      console.log("connected to DB");
-    });
-    // on SIGINT signal exist
-    process.on("SIGINT", () => {
-      mongoose.connection.close(() => {
-        console.log("Mongoose disconnected on app termination");
-        process.exit(0);
-      });
-    });
-  }
-
-  createServer() {
-    http.createServer(this.#app).listen(this.#PORT);
-    if (process.env.app_state == "dev") {
-      console.log(`run on > http://localhost:${this.#PORT}`);
-    }
-  }
-
-  createRoute() {
-    this.#app.use("/", allRoutes);
-  }
-}
-
-export default Application;
